94e2310d74e6171955e8b5b48c90796ef6e35ccf
ktn9 <khenghau.tan@3ds.com>
2020-06-02 19:06:23 +0800

    middle Widget , 1st row done

--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -7,6 +7,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String dropdownvalue = 'items1';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,62 +16,97 @@ class _HomePageState extends State<HomePage> {
         title: Text("vehicle"),
         backgroundColor: Colors.red,
       ),
-      body: Container(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: DefaultTabController(
-                  length: 3,
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: DefaultTabController(
+                    length: 3,
+                    child: Container(
+                      color: Colors.red[400],
+                      child: Column(
+                        children: [
+                          TabBar(
+                            indicatorColor: Colors.white,
+                            tabs: [
+                              Tab(text: "All"),
+                              Tab(text: "Online"),
+                              Tab(text: "Offline")
+                            ],
+                          ),
+                          Expanded(
+                              child: TabBarView(
+                            children: <Widget>[
+                              Container(
+                                  color: Colors.white,
+                                  child: ListView(
+                                    children: list,
+                                  )),
+                              Container(
+                                  color: Colors.white,
+                                  child: ListView(
+                                    children: list,
+                                  )),
+                              Container(
+                                  color: Colors.white,
+                                  child: ListView(
+                                    children: list,
+                                  )),
+                            ],
+                          )),
+                        ],
+                      ),
+                    )),
+              ),
+              Expanded(
+                  child: Column(children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 20, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Row(children: <Widget>[
+                          Container(
+                            height: 50,
+                            child: Center(child: Text("Group By:")),
+                          ),
+                          SizedBox(width: 20),
+                          DropdownButton<String>(
+                              value: dropdownvalue,
+                              items: <String>['items1', 'items2', 'items3']
+                                  .map((String value) {
+                                return new DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (String newValue) {
+                                setState(() {
+                                  dropdownvalue = newValue;
+                                });
+                              }),
+                        ]),
+                      ),
+                      Icon(Icons.settings)
+                    ],
+                  ),
+                ),
+              ])),
+              Expanded(
                   child: Container(
-                    color: Colors.red[400],
-                    child: Column(
-                      children: [
-                        TabBar(
-                          indicatorColor: Colors.white,
-                          tabs: [
-                            Tab(text: "All"),
-                            Tab(text: "Online"),
-                            Tab(text: "Offline")
-                          ],
-                        ),
-                        Expanded(
-                            child: TabBarView(
-                          children: <Widget>[
-                            Container(
-                                color: Colors.white,
-                                child: ListView(
-                                  children: list,
-                                )),
-                            Container(
-                                color: Colors.white,
-                                child: ListView(
-                                  children: list,
-                                )),
-                            Container(
-                                color: Colors.white,
-                                child: ListView(
-                                  children: list,
-                                )),
-                          ],
-                        )),
-                      ],
-                    ),
-                  )),
-            ),
-            Expanded(
-                child: Container(
-              color: Colors.brown,
-            )),
-            Expanded(
-                child: Container(
-              color: Colors.cyan,
-            ))
-          ],
+                color: Colors.cyan,
+              ))
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: CurvedNavigationBar(
         color: Colors.red,
-        height: 55,
+        height: 45,
         backgroundColor: Colors.white,
         items: <Widget>[
           Icon(
