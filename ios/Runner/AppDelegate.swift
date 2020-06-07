b59405d1d25e377dff8cdf78c4b8f8c1e67e1735
ktn9 <khenghau.tan@3ds.com>
2020-06-07 16:40:39 +0800

    right panel UI done

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBVeOnyZeMQ_z3PdxMHL2kQzu38Yrw7RzE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_google_maps/flutter_google_maps.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,7 +8,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GlobalKey<GoogleMapStateBase> _key = GlobalKey<GoogleMapStateBase>();
   String dropdownvalue = 'items1';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +25,7 @@ class _HomePageState extends State<HomePage> {
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              // Left Panel
               Expanded(
                 flex: 2,
                 child: DefaultTabController(
@@ -62,6 +66,7 @@ class _HomePageState extends State<HomePage> {
                       ),
                     )),
               ),
+              // Middle Panel
               Expanded(
                   flex: 6,
                   child: Column(children: <Widget>[
@@ -180,10 +185,229 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ])),
+              // Right Panel
               Expanded(
                   flex: 2,
-                  child: Container(
-                    color: Colors.cyan,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Container(
+                        height: 300,
+                        width: 300,
+                        child: GoogleMap(
+                          key: _key,
+                          initialZoom: 12,
+                          mapType: MapType.roadmap,
+                          initialPosition:
+                              GeoCoord(3.1711885963699276, 101.6655700046132),
+                        ),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.only(left: 15),
+                                child: Column(children: [
+                                  Image(
+                                    height: 100,
+                                    width: 100,
+                                    image: NetworkImage(
+                                        'https://c8.alamy.com/comp/P6W7NW/blue-mercedes-benz-actros-tank-truck-of-kuljetusliike-markus-hanninen-oy-hauls-load-along-highway-on-sunny-day-of-summer-salo-finland-july-1-2018-P6W7NW.jpg'),
+                                  ),
+                                  Container(
+                                      height: 150,
+                                      width: 100,
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          CircleAvatar(
+                                            backgroundColor: Colors.green,
+                                            radius: 5,
+                                          ),
+                                          SizedBox(width: 10),
+                                          Text("Online")
+                                        ],
+                                      ))
+                                ]),
+                              ),
+                              Container(
+                                  height: 250,
+                                  width: 200,
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                          width: 170,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(height: 5),
+                                              Text(
+                                                "Name",
+                                                style: TextStyle(fontSize: 10),
+                                              ),
+                                              Text("KAMAZ")
+                                            ],
+                                          )),
+                                      Container(
+                                          margin: EdgeInsets.only(top: 5),
+                                          width: 170,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(height: 5),
+                                              Text(
+                                                "License Plate ",
+                                                style: TextStyle(fontSize: 10),
+                                              ),
+                                              Text("197TYS")
+                                            ],
+                                          )),
+                                      Container(
+                                          margin: EdgeInsets.only(top: 5),
+                                          width: 170,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(height: 5),
+                                              Text(
+                                                "Status",
+                                                style: TextStyle(fontSize: 10),
+                                              ),
+                                              Text("StandStill")
+                                            ],
+                                          )),
+                                      Container(
+                                          margin: EdgeInsets.only(top: 5),
+                                          width: 170,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(height: 5),
+                                              Text(
+                                                "Fuel ",
+                                                style: TextStyle(fontSize: 10),
+                                              ),
+                                              Text("—")
+                                            ],
+                                          )),
+                                      Container(
+                                          margin: EdgeInsets.only(top: 5),
+                                          width: 170,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(height: 5),
+                                              Text(
+                                                "Mileage ",
+                                                style: TextStyle(fontSize: 10),
+                                              ),
+                                              Text("51313.70km")
+                                            ],
+                                          )),
+                                      Container(
+                                          margin: EdgeInsets.only(top: 5),
+                                          width: 170,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(height: 5),
+                                              Text(
+                                                "Insurance ",
+                                                style: TextStyle(fontSize: 10),
+                                              ),
+                                              Text("—")
+                                            ],
+                                          )),
+                                    ],
+                                  ))
+                            ],
+                          ),
+                          SingleChildScrollView(
+                              child: Column(
+                            children: <Widget>[
+                              Container(
+                                  child: Row(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.only(left: 15),
+                                    child: Column(children: [
+                                      Image(
+                                        height: 100,
+                                        width: 100,
+                                        image: NetworkImage(
+                                            'https://c8.alamy.com/comp/P6W7NW/blue-mercedes-benz-actros-tank-truck-of-kuljetusliike-markus-hanninen-oy-hauls-load-along-highway-on-sunny-day-of-summer-salo-finland-july-1-2018-P6W7NW.jpg'),
+                                      ),
+                                      Container(
+                                          height: 30,
+                                          width: 100,
+                                          child: Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              CircleAvatar(
+                                                backgroundColor: Colors.green,
+                                                radius: 5,
+                                              ),
+                                              SizedBox(width: 10),
+                                              Text("Online")
+                                            ],
+                                          ))
+                                    ]),
+                                  ),
+                                  Container(
+                                      margin: EdgeInsets.only(right: 5),
+                                      height: 100,
+                                      width: 200,
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                              width: 170,
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  SizedBox(height: 5),
+                                                  Text(
+                                                    "Name",
+                                                    style:
+                                                        TextStyle(fontSize: 10),
+                                                  ),
+                                                  Text("KAMAZ")
+                                                ],
+                                              )),
+                                          Container(
+                                              margin: EdgeInsets.only(top: 5),
+                                              width: 170,
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  SizedBox(height: 5),
+                                                  Text(
+                                                    "License Plate ",
+                                                    style:
+                                                        TextStyle(fontSize: 10),
+                                                  ),
+                                                  Text("197TYS")
+                                                ],
+                                              )),
+                                        ],
+                                      ))
+                                ],
+                              )),
+                            ],
+                          ))
+                        ],
+                      )
+                    ],
                   ))
             ],
           ),
